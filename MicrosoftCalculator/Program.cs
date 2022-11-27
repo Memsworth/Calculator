6d3102183968faa00757e8b3064e2b2c64c89dd9
Abubakar Ahmed <abubakarahmed4497@outlook.com>
2022-11-28 01:27:58 +0900

    Cleaned up files

--- MicrosoftCalculator/Program.cs
+++ MicrosoftCalculator/Program.cs
@@ -1,38 +1,37 @@
-﻿using CalculatorLibrary;
+﻿using Newtonsoft.Json;
 
 namespace MicrosoftCalculator
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
             var calculator = new Calculator();
             bool endApp = false;
-            
+
             // Display title as the C# console calculator app.
             Console.WriteLine("Console Calculator in C#\r");
             Console.WriteLine("------------------------\n");
 
             while (!endApp)
             {
-                // Declare variables and set to empty.
                 double cleanNum1;
                 double cleanNum2;
-
+                
                 if (calculator.CalculationList.Any())
                 {
-                    // Ask the user to type the first number.
-                    cleanNum1 = GetInput("Type a number, and then press Enter: ", calculator.CalculationList);
-                    cleanNum2 = GetInput("Type another number, and then press Enter: ", calculator.CalculationList);
+                    cleanNum1 = Helper.GetInput("Type a number, and then press Enter: ", calculator.CalculationList);
+                    cleanNum2 = Helper.GetInput("Type another number, and then press Enter: ", calculator.CalculationList);
                 }
+
                 else
                 {
                     Console.Write("Type a number, and then press Enter: ");
-                    cleanNum1 = CleanNum();
+                    cleanNum1 = Helper.CleanNum();
                     Console.Write("Type another number, and then press Enter: ");
-                    cleanNum2 = CleanNum();
+                    cleanNum2 = Helper.CleanNum();
                 }
-                
+
                 ShowMenu();
 
                 string op = Console.ReadLine();
@@ -49,7 +48,7 @@ namespace MicrosoftCalculator
                         Console.WriteLine("Your result: {0:0.##}\n", result);
                         calculator.IncreaseCount();
                         calculator.CalculationList.Add(result);
-                        ClearList(calculator.CalculationList);
+                        Helper.ClearList(calculator.CalculationList);
                     }
                 }
                 catch (Exception e)
@@ -59,38 +58,15 @@ namespace MicrosoftCalculator
 
                 Console.WriteLine("------------------------\n");
 
-                // Wait for the user to respond before closing.
                 Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
                 if (Console.ReadLine() == "n") endApp = true;
 
-                Console.WriteLine("\n"); // Friendly linespacing.
+                Console.WriteLine("\n");
             }
-            
+
             calculator.Finish();
             return;
         }
-
-        private static void ClearList(List<double> list)
-        {
-            Console.Write("\nDo you want to clear your list? (Y/y) ");
-            if (GetApproval())
-            {
-                list.Clear();
-            }
-        }
-        private static double GetInput(string message, List<double> list)
-        {
-            Console.WriteLine("Do you want to take from list (Y/y): ");
-            if (GetApproval())
-            {
-                return GetListItem(message, list);
-            } 
-            
-            Console.Write(message); 
-            return CleanNum();
-        }
-
-        private static bool GetApproval() => (Console.ReadLine() == "y" || Console.ReadLine() == "Y");
 
         private static void ShowMenu()
         {
@@ -102,24 +78,11 @@ namespace MicrosoftCalculator
             Console.WriteLine("\td - Divide");
             Console.Write("Your option? ");
         }
+    }
 
-        private static double GetListItem(string message, List<double> calculatorCalculationList)
-        {
-            int indexInput;
-            Console.Write(message);
-            foreach(var item in calculatorCalculationList)
-            {
-                Console.Write($"{item} -  ");
-            }
-            Console.Write("Enter index: ");
-            while (!int.TryParse(Console.ReadLine(), out indexInput) && calculatorCalculationList.Contains(calculatorCalculationList[indexInput]))
-            {
-                Console.WriteLine("item doesn't exist in list. Enter index again");
-            }
-
-            return calculatorCalculationList[indexInput];
-        }
-        private static double CleanNum()
+    public static class Helper
+    {
+        public static double CleanNum()
         {
             double cleanNum;
             while (!double.TryParse(Console.ReadLine(), out cleanNum))
@@ -128,6 +91,124 @@ namespace MicrosoftCalculator
             }
 
             return cleanNum;
+        }
+
+        private static double GetListItem(string message, List<double> calculatorCalculationList)
+        {
+            int indexInput;
+            Console.Write(message);
+            foreach (var item in calculatorCalculationList)
+            {
+                Console.Write($"{item} -  ");
+            }
+
+            Console.Write("Enter index: ");
+            while (!int.TryParse(Console.ReadLine(), out indexInput) &&
+                   calculatorCalculationList.Contains(calculatorCalculationList[indexInput]))
+            {
+                Console.WriteLine("item doesn't exist in list. Enter index again");
+            }
+
+            return calculatorCalculationList[indexInput];
+        }
+
+        private static bool GetApproval() => (Console.ReadLine() == "y" || Console.ReadLine() == "Y");
+
+        public static double GetInput(string message, List<double> list)
+        {
+            Console.WriteLine("Do you want to take from list (Y/y): ");
+            if (GetApproval())
+            {
+                return GetListItem(message, list);
+            }
+
+            Console.Write(message);
+            return CleanNum();
+        }
+
+        public static void ClearList(List<double> list)
+        {
+            Console.Write("\nDo you want to clear your list? (Y/y) ");
+            if (GetApproval())
+            {
+                list.Clear();
+            }
+        }
+    }
+
+    public class Calculator
+    {
+        private JsonWriter writer;
+        public List<double> CalculationList { get; private set; }
+        public int CalculationCount { get; private set; } = 0;
+
+        public Calculator()
+        {
+            CalculationList = new List<double>();
+            StreamWriter logFile = File.CreateText("calculatorlog.json");
+            logFile.AutoFlush = true;
+            writer = new JsonTextWriter(logFile);
+            writer.Formatting = Formatting.Indented;
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operations");
+            writer.WriteStartArray();
+        }
+
+        public void IncreaseCount() => CalculationCount++;
+
+        public double DoOperation(double num1, double num2, string op)
+        {
+            double
+                result = double
+                    .NaN; // Default value is "not-a-number" if an operation, such as division, could result in an error.
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operand1");
+            writer.WriteValue(num1);
+            writer.WritePropertyName("Operand2");
+            writer.WriteValue(num2);
+            writer.WritePropertyName("Operation");
+
+            // Use a switch statement to do the math.
+            switch (op)
+            {
+                case "a":
+                    result = num1 + num2;
+                    writer.WriteValue("Add");
+                    break;
+                case "s":
+                    result = num1 - num2;
+                    writer.WriteValue("Subtract");
+                    break;
+                case "m":
+                    result = num1 * num2;
+                    writer.WriteValue("Multiply");
+                    break;
+                case "d":
+                    // Ask the user to enter a non-zero divisor.
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                    }
+
+                    writer.WriteValue("Divide");
+                    break;
+                // Return text for an incorrect option entry.
+                default:
+                    break;
+            }
+
+            writer.WritePropertyName("Result");
+            writer.WriteValue(result);
+            writer.WriteEndObject();
+
+            return result;
+        }
+
+        public void Finish()
+        {
+            writer.WriteEndArray();
+            writer.WriteEndObject();
+            writer.Close();
         }
     }
 }
